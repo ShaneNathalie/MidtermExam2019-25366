acfd4035a8578cfdc2a6fb44f83fbfcd8cd0b579
Shane02Nathalie <shane04nathalie@gmail.com>
2021-10-27 04:49:21 +0700

    Commit Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'contacts',component:ContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
